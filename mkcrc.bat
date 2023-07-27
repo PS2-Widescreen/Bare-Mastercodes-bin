b796ef2c2ac85445740661c97253fdc066b829be
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-07-27 13:01:55 -0300

    tweak mkcrc.bat to use new PCSX2 repository

--- mkcrc.bat
+++ mkcrc.bat
@@ -1,14 +1,17 @@
 @echo off
 
-if exist CRCLIST.TXT del CRCLIST.TXT
-
-cd MASTERCODES
-for %%a in (*.cht) do (
-
-	for /f "delims=" %%b in ('type %%a ^| findstr "CRC" ^| busybox grep -Eo "[a-fA-F0-9]{8}"') do echo %%~na;%%b >> ..\CRCLIST.TXT
+if not exist CRCLIST.TXT (
+	cd MASTERCODES
+	for %%a in (*.cht) do (
+		for /f "delims=" %%b in ('type %%a ^| findstr "CRC" ^| busybox grep -Eo "[a-fA-F0-9]{8}"') do echo %%~na;%%b >> ..\CRCLIST.TXT
+	)
+	cd ..
 )
+
+
+
 echo completed CRC list. looking for matching CRC on PNACH...
 timeout 5
-for /f "delims=; tokens=1,2*" %%a in (CRCLIST.TXT) do if exist ..\pnach-DB\PNACH\%%b.pnach echo %%b.pnach;%a
+for /f "delims=; tokens=1,2*" %%a in (CRCLIST.TXT) do if exist ..\pcsx2_patches\patches\%%b.pnach echo %%b.pnach;%a
 pause
 pause
